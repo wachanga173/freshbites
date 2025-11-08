7ac6c03af6151d9c9d752004681e7b9f563b82d6
wachanga173 <wachangapeter763@gmail.com>
2025-11-08 18:23:11 +0300

    Simplify Vercel build configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,13 +14,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom']
-        }
-      }
-    }
+    sourcemap: false
   }
 })
