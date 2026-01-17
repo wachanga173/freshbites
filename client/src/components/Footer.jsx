b897bde54ff13c5c722f076e57edde4a83d20d82
wachanga173 <wachangapeter763@gmail.com>
2026-01-17 15:05:32 +0300

    feat: Update PWA install button text to indicate iOS & Desktop support

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -167,7 +167,7 @@ export default function Footer() {
                     <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4M7 10l5 5 5-5M12 15V3"/>
                   </svg>
                   <div className="download-text">
-                    <small>Quick Install</small>
+                    <small>For iOS & Desktop</small>
                     <strong>Install App Now</strong>
                   </div>
                 </button>
